dd32295f4e2e85c24e21b19e59c85b747e7753d3
adita24si <adita24si@mahasiswa.pcr.ac.id>
2026-04-15 14:41:02 +0700

    tugas p5

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 
 
@@ -8,4 +9,10 @@ export default defineConfig({
   plugins: [react(),
      tailwindcss(),
     ]
+=======
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+>>>>>>> 3c94461 (tugasp5 pertemuan 5)
 })
